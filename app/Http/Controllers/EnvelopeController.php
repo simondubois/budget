94339716178b082613b2f2b77a7f87a3b489bef6
Simon Dubois <simon@dubandubois.com>
2019-08-06 12:25:16 +0200

    Implement envelope API

---
+++ app/Http/Controllers/EnvelopeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Envelope;
+use App\Http\Resources\EnvelopeCollection;
+use App\Http\Resources\EnvelopeResource;
+use Illuminate\Http\Request;
+
+class EnvelopeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new EnvelopeCollection(
+            Envelope::orderBy('name')->get()
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Envelope  $envelope
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Envelope $envelope)
+    {
+        return new EnvelopeResource(
+            $envelope
+        );
+    }
+}
