99d3122f04e78903c0cd0c80e9d766e7f65d72f0
Simon Dubois <simon@dubandubois.com>
2019-08-12 12:28:45 +0200

    Setup frontend dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.webpackConfig({
+    plugins: [
+        new (require("webpack").ContextReplacementPlugin)(/moment[/\\]locale$/, /en|fr/),
+    ],
+});
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
