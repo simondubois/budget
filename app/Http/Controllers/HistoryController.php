7dc13f5a487c36de14c5e6b4b3f8dc10a1251d2f
Simon Dubois <simon@dubandubois.com>
2019-09-02 17:35:30 +0200

    Implement account history API

---
+++ app/Http/Controllers/HistoryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\MoneyResource;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+
+abstract class HistoryController extends Controller
+{
+    /**
+     * Build period array from period string.
+     *
+     * @param  Collection  $dates
+     * @return Collection
+     */
+    protected function computeDates(Collection $dates) : Collection
+    {
+        return $dates
+            ->combine($dates)
+            ->map(function (string $date) : array {
+                $dateParts = explode('-', $date);
+                if (count($dateParts) === 1) {
+                    return [
+                        Carbon::create($dateParts[0])->startOf('year'),
+                        Carbon::create($dateParts[0])->endOf('year'),
+                    ];
+                }
+                return [
+                    Carbon::create($dateParts[0], $dateParts[1])->startOf('month'),
+                    Carbon::create($dateParts[0], $dateParts[1])->endOf('month'),
+                ];
+            });
+    }
+
+    /**
+     * Build response from aggregate list.
+     *
+     * @param  array  $history
+     * @return Collection
+     */
+    protected function formatResponse(array $history) : Collection
+    {
+        return collect($history)
+            ->map(function (Collection $aggregates) : Collection {
+                return $aggregates->mapInto(MoneyResource::class);
+            });
+    }
+}
