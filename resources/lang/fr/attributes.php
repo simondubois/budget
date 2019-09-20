86b348912754e34197acf01c93a3a2c79425f23f
Simon Dubois <simon@dubandubois.com>
2019-09-20 07:02:20 +0200

    Implement envelope API (create & update)

--- resources/lang/fr/attributes.php
+++ resources/lang/fr/attributes.php
@@ -8,4 +8,11 @@ return [
         'bank' => 'banque',
     ],
 
+    'envelope' => [
+        'name' => 'nom',
+        'icon' => 'icône',
+        'default_allocation_amount' => 'montant de l\'allocation par défaut',
+        'default_allocation_currency_iso' => 'devise de l\'allocation par défaut',
+    ],
+
 ];
