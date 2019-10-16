3399ccf337ceebd33e83a582391ab9fe5124ecb3
Simon Dubois <simon@dubandubois.com>
2019-10-16 12:38:27 +0200

    Implement seed route for demo

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('fetch-rates', function () {
     Artisan::call('rate:fetch', [], $output);
     return nl2br($output->fetch());
 });
+
+Route::get('seed', function () {
+    abort_unless(env('APP_ENV') === 'demo', 404);
+    $output = new Symfony\Component\Console\Output\BufferedOutput();
+    Artisan::call('migrate:refresh', ['--seed' => true], $output);
+    return nl2br($output->fetch());
+});
